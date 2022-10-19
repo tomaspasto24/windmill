cdfb111f281d80070f03e4ae12e4d5ccb153ef5f
Tomas <tomaspasto24@gmail.com>
2022-10-18 21:03:24 -0300

    .

--- src/app/login-screen/login-screen.component.ts
+++ src/app/login-screen/login-screen.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-screen',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginScreenComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  login(email: string, password: string) {
+    this.router.navigate(['/works']);
   }
 
 }
